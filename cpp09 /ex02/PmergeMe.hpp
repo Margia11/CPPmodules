53f160f89c84fa43f2dfd667c4ac7fc88909cea8
Andrea Margiacchi <121580748+Margia11@users.noreply.github.com>
2024-02-01 10:49:25 +0100

    Add files via upload

--- cpp09 /ex02/PmergeMe.hpp
+++ cpp09 /ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: andreamargiacchi <andreamargiacchi@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:44:48 by andreamargi       #+#    #+#             */
-/*   Updated: 2023/11/08 17:29:25 by andreamargi      ###   ########.fr       */
+/*   Updated: 2023/11/02 16:45:02 by andreamargi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,7 @@
 
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <vector>
-#include <list>
-#include <ctime>
-#include <set>
-#include <iomanip>
 
 
-
-
-class PmergeMe
-{
-	private:
-		std::set<std::string> data;
-		std::vector<int> v;
-		std::list<int> l;
-	public:
-		PmergeMe();
-		PmergeMe(const PmergeMe& src);
-		~PmergeMe();
-		PmergeMe& operator=(const PmergeMe& src);
-		void FordJohngoddog(std::vector<int> &data);
-		void FordJohngoddogs(std::list<int> &data);
-		void SortVectorandgetTime(std::vector<int>& data, int argc);
-		void SortListandgetTime(std::list<int>& data);
-};
 
 #endif
